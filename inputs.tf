b7ba9db072af9836d6adaa8f6fe1619f5cc2b1cb
Jorge Alzati <alzati.jorge@gmail.com>
2019-04-23 17:59:57 -0700

    Last checking for today

--- inputs.tf
+++ inputs.tf
@@ -2,3 +2,7 @@ variable "webserver_port" {
   description = "The port the web servers will listen"
   default = 8080
 }
+
+variable "region" {
+  description = "Region where the infrastructure will be created"
+}
