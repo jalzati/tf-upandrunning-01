df13d51c6a09b0e7f5e48741088a0780810086c3
Jorge Alzati <alzati.jorge@gmail.com>
2019-04-28 12:27:08 -0700

    Added code to create VPC, subnets, etc.

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,21 @@
 resource "aws_security_group" "sg-webserver" {
   name = "Web Server"
+  vpc_id = "${aws_vpc.webserver_vpc.id}"
 
   ingress {
     from_port = "${var.webserver_port}"
     protocol = "tcp"
     to_port = "${var.webserver_port}"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP connections from anywhere in the Internet"
+  }
+
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["${var.vpnip}"]
+    description = "Allow SSH connections from only the VPN server"
   }
 
   tags {
@@ -13,16 +23,35 @@ resource "aws_security_group" "sg-webserver" {
     Owner = "jalzati@anomali.com"
     Department = "DevOps"
     Environment = "research"
-    ServerPort = "${var.webserver_port}"
   }
+}
+
+resource "aws_eip" "dev_elasticip" {
+  vpc = true
+  count = "${var.nwebservers}"
+  instance = "${element(aws_instance.webserver.*.id,count.index)}"
+
+  tags {
+    Name = "webserver-${count.index}"
+    Owner = "${var.owner}"
+    Department = "${var.department}"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_key_pair" "webserver_keypair" {
+  key_name   = "${var.keyname}"
+  public_key = "${file(var.public_key_path)}"
 }
 
 resource "aws_instance" "webserver" {
 
-  ami = "ami-03d12de7d0e87fbf3"
-  instance_type = "t2.micro"
+  ami = "${var.ami}"
+  instance_type = "${var.ec2type}"
   vpc_security_group_ids = ["${aws_security_group.sg-webserver.id}"]
-  count = 2
+  count = "${var.nwebservers}"
+  key_name = "${var.keyname}"
+  subnet_id = "${aws_subnet.webserver_public_subnet_3.id}"
 
     tags {
       Name = "webserver-${count.index}"
@@ -31,10 +60,10 @@ resource "aws_instance" "webserver" {
       Environment = "research"
     }
   
-  user_data = <<-EOF
+  user_data = <<EOF
 #!/bin/bash
 echo "Hello, World" > index.html
 nohup busybox httpd -f -p 8080 &
 EOF
-  
+
 }
--- inputs.tf
+++ inputs.tf
@@ -1,7 +1,54 @@
+variable "region" {
+  description = "Region where the web servers will be created"
+}
+
 variable "webserver_port" {
   description = "The port the web servers will listen"
 }
 
-variable "region" {
-  description = "Region where the infrastructure will be created"
+variable "keyname" {
+  description = "Public key to connect to the web servers"
 }
+
+variable "public_key_path" {
+  description = "Path to the public key for the web servers"
+}
+
+variable "owner" {
+  description = "Email address of the web servers owner"
+}
+
+variable "department" {
+  description = "Department responsible for the web servers"
+}
+
+variable "environment" {
+  description = "Environment that these web servers will be part of"
+}
+
+variable "vpnip" {
+  description = "IP address of the VPN server"
+}
+
+variable "ami" {
+  description = "AMI to be used to create the web servers"
+}
+
+variable "ec2type" {
+  description = "Type of EC2 instance for the web servers"
+}
+
+variable "nwebservers" {
+  description = "Number of web servers that will be created"
+}
+
+variable "vpc_cidr" {
+  description = "Address space assigned to this VPC"
+}
+
+variable "subnet_cidrs" {
+  description = "Address space assigned to the subnets where the web servers will be created"
+  type = "list"
+}
+
+data "aws_availability_zones" "available_azs" {}
